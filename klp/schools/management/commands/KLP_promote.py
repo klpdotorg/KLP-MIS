bf801d99cbdab2fdfb8686125550f159590c15d3
sree <sree@sree-laptop.(none)>
2012-11-26 04:40:53 +0530

    Created 3.0 branch

--- klp/schools/management/commands/KLP_promote.py
+++ klp/schools/management/commands/KLP_promote.py
@@ -27,16 +27,26 @@ class Command(BaseCommand):
 		         institutions = Institution.objects.filter(cat__categoryType=1, active=2) 
 		    for inst in institutions:
 			    ''' Filter For Active StudentGroups Of type Class '''
-			    studentGroups = StudentGroup.objects.filter(institution=inst, active=2, group_type="Class").order_by("name").reverse()
+			    studentGroups = StudentGroup.objects.filter(institution=inst, active__in=[1,2], group_type="Class").order_by("name").reverse()
 			    sgdic= studentGroups.values_list('id','name')
-			    sgidlist=dict(((int(k[1]),k[0]) for k in sgdic))
-			    sglist=[int(k[1]) for k in sgdic]
+			    sgidlist={}
+			    for k in sgdic:
+  				 if sgidlist.has_key(int(k[1])):
+      					val= sgidlist[int(k[1])]
+      					val.append(k[0])
+   				 else:
+     					 val=[k[0]]
+   				 sgidlist[int(k[1])]=val
+			   
+			    sglist=list(set([int(k[1]) for k in sgdic]))
 			    sglist.sort()
                             sglist.reverse()
 			    totalsg=len(sglist)
 			    for sgid in sglist:
-			       
-				        sg=StudentGroup.objects.get(id=sgidlist[sgid])
+			        for k in sgidlist[sgid]:
+			        
+				        sg=StudentGroup.objects.get(id=k) 
+				        print "In student group [%s]: %s %s"%(sg.id, sg.name,sg.section)
 				        ''' Filter For Active Student And StudentGroup Relation Objects Of Current Academic Year '''
 				        sg_stRealtions = Student_StudentGroupRelation.objects.filter(student_group=sg, academic=currentAcademicObj, active=2)
 				        '''  school have next class or not .If next class is there ,create or get StudentGroup '''
@@ -44,33 +54,45 @@ class Command(BaseCommand):
                                                 groupName = sg.name
                                                 nxtGroup = int(groupName) + 1
                                                 groupSec = sg.section
-                                                '''Checks For Next Class With Section of type class. if not there it will create '''
-				                nextSg = StudentGroup.objects.get_or_create(institution = inst, name=nxtGroup, section=groupSec, active__in= [0,1,2], group_type="Class")
-                                                nextSg=nextSg if not type(nextSg) is tuple else nextSg[0]
-                                                '''check next class active or not .if not active ,it will activate'''
-                                                if nextSg.active!=2:
-				                  nextSg.active=2
-				                  nextSg.save()               
-						'''Student Group  has student or not .student are not there ,deactive the student group else promote the student'''
+				                #print "In student group [%s]: %s %s"%(sg.id, sg.name,sg.section)
+
 						if sg_stRealtions:
-						    for sg_st in sg_stRealtions:
+                                                	'''Checks For Next Class With Section of type class. if not there it will create '''
+				                	nextSg = StudentGroup.objects.get_or_create(institution = inst, name=nxtGroup, section=groupSec, active__in= [0,1,2], group_type="Class")
+                                                	nextSg=nextSg if not type(nextSg) is tuple else nextSg[0]
+                                                	#print groupSec,groupName						
+						    	''' if current yeas student group has students, then before you promote them to next year frst make their student group active fro next year '''
+			                                nextSg.active=2
+			                                nextSg.save()  
+					                print "Next year student group [%s] activated" %(sg.id)  
+						            
+						    	for sg_st in sg_stRealtions:
 						                studentObj = sg_st.student
 						                '''Checks For Next Class With Section of type class. If it There map with student for next academic else pass'''
-	      					                nxt_Sg_relation = Student_StudentGroupRelation.objects.get_or_create(student_group=nextSg, student=studentObj, academic=nextAcademicObj , active=2)
+	      					                nxt_Sg_relation = Student_StudentGroupRelation.objects.get_or_create(student_group=nextSg, student=studentObj, academic=nextAcademicObj )
+	      					                nxt_Sg_relation.active =2
+	      					                nxt_Sg_relation.save()
+	      					                sg_st.active=1 #
+	      					                sg_st.save()
+	      					                print "Student %s promoted from %s to %s" %(studentObj,sgid,nextSg)
 						else:
                                                     '''No students are there ,student group will be deactivated'''
                                                     sg_stRealtions.update(active=1)
-						    sg.active=1 #Not clear it should be delete 0 or inactive 1
-						    sg.save()
+					            '''sg.active=1 #inactive 1
+					            sg.save()
+					            print "Current student group [%s] deactivated" %(sg.id)'''
+                                                    
+						            
 		                        else:
                                           '''It student group is final ,change the action to 4 .that means all promoted from school'''
                                           
                                           
 		                          sg_stRealtions.update(active=4)
+                                  
 		    self.stdout.write('Students Are Promoted ...\n')
 		    '''To find the all current year programees'''
 		    prog=Programme.objects.filter(endDate__range=[datetime.strptime(str(currentYear)+'-06-01','%Y-%m-%d'),datetime.strptime(str(currentYear+1)+'-05-31','%Y-%m-%d')])
 		    #assement=Assessment.objects.filter(programme__in=prog)
 		    prog.update(active=1)
-		    print prog
+		    
 		    self.stdout.write('Programmes are inActivated ...\n') 
