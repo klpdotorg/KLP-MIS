27a788345d6ad564e54f089ffe9853b94d4adc93
sree <sree@sree-laptop.(none)>
2012-12-10 17:55:07 +0530

    New structure as suggested in Django 1.4.x has been putin by reorganising the code and associated directories...

--- klp/fullhistory/__init__.py
+++
@@ -1,2 +0,0 @@
-VERSION = (0,3,0,'final')
-from fullhistory import register_model, get_active_histories
--- klp/fullhistory/signals.py
+++
@@ -1,4 +0,0 @@
-from django import dispatch
-
-post_create = dispatch.Signal()
-post_adjust = dispatch.Signal()
