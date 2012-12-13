150ed71e1d6bfdf012bf638f19922aa5d5d494e8
basavaraj <basavaraj.hiremath@mahiti.org>
2012-12-13 19:40:18 +0530

    Getting Login page

--- klpmis/klpmis/urls.py
+++ klpmis/klpmis/urls.py
@@ -1,28 +1,18 @@
 from django.conf.urls import patterns, include, url
-
+from django.contrib import admin
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    #url(r'^$', 'klpmis.views.home', name='home'),
-    #url(r'^klpmis/', include('klpmis.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    #url(r'^admin/', include(admin.site.urls)),
-    # Uncomment the admin/doc line below and add
-    #'django.contrib.admindocs'
-    # to INSTALLED_APPS to enable admin documentation:
-    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    #(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'klprestApi.AuthenticationApi.KLP_Login'),
+    url(r'^home/', 'klprestApi.HomeApi.KLP_Home'),
+    url(r'^admin/', include(admin.site.urls)),
+    
     (r'^static_media/(?P<path>.*)$', 'django.views.static.serve',
     {'document_root': 'static_media'}),
+
     url(r'', include('klprestApi.HomeApi')),
     url(r'', include('klprestApi.TreeMenu')),
     url(r'', include('klprestApi.BoundaryApi')),
