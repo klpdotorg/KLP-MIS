b75b5da452c73a56234616b04e1dd737c70f0791
mahiti <mahiti@mahiti-desktop.(none)>
2013-08-02 22:54:24 +0530

    Handeled the exception for assessment answer screen

--- klpmis/klprestApi/AnswerApi.py
+++ klpmis/klprestApi/AnswerApi.py
@@ -33,7 +33,6 @@ class KLP_ChangeAns(Resource):
     def read(self, request):
 
         user = request.user  # get Logged in user
-        print "logged user is ", user
         userid = user.id
         student = request.POST.get('student')  #  get Student
         programId = request.POST.get('programId')  #  get programme id
@@ -61,7 +60,7 @@ class KLP_ChangeAns(Resource):
 
             studentObj = studentObj1[0]
         elif assessmentObj.typ == 2:
-            modelName='student group' 
+            modelName='studentgroup' 
             studentObj = student_groupObj[0]
             instObj = studentObj.institution
         
@@ -74,7 +73,7 @@ class KLP_ChangeAns(Resource):
         Questions_list = \
             Question.objects.filter(assessment__id=assessmentId)  # get questions under assessment
         dobleEntReq = assessmentObj.double_entry
-        #pdb.set_trace()
+        
         if dobleEntReq:
             dE = 2 
         else:
@@ -90,7 +89,7 @@ class KLP_ChangeAns(Resource):
         for (index, question) in enumerate(Questions_list):
             textField = 'student_%s_%s' % (str(question.id), student)
             textFieldVal = request.POST.get(textField)  # get each text field values
-        
+            
             primaryfield = 'primaryvalue_%s' % student
             primaryfieldVal = request.POST.get(primaryfield, '')
             ansIdfield = 'ansId_%s_%s' % (str(question.id), student)
@@ -98,10 +97,10 @@ class KLP_ChangeAns(Resource):
 
         
         
-
+            ansObjs = []
             # If answer object already exists update data.
-        
-            ansObjs=Answer.objects.filter(id=ansIdfieldVal)
+            if ansIdfieldVal:
+                ansObjs=Answer.objects.filter(id=ansIdfieldVal)
             try:
                 ansObj =ansObjs[0]  # .defer("user1","user2")[0]
             except:
@@ -172,7 +171,7 @@ class KLP_ChangeAns(Resource):
                         ansObj.double_entry = 2
                         ansObj.last_modified_by = user
                         ansObj.user2 = user
-                    #pdb.set_trace()
+                    
                     try:
                         
                         newanswerdata['form-0-answer_score']=ansObj.answer_score
@@ -188,25 +187,36 @@ class KLP_ChangeAns(Resource):
                         newanswerdata['form-0-user1']=ansObj.user1.id
                         if ansObj.user2:
                             newanswerdata['form-0-user2']=ansObj.user2.id #.id
-                            print " the user is **********", ansObj.user2.id
                         else:
                             newanswerdata['form-0-user2']=''
                         
                         newanswerdata['form-0-id']=ansObj.id  
                         newanswerdata['form-INITIAL_FORMS']=1
-                        #print newanswerdata,ansObjs[0].id 
+
                         request.POST=newanswerdata
 
                         firstuserdata1 = Answer.objects.filter(question__id=\
                         question.id, user1__id = user.id, object_id = \
-                        studentObj.id, flexi_data = primaryfieldVal)
+                        studentObj.id,id=ansObj.id)
+
+                        ansflexivals = Answer.objects.filter(question__id=\
+                        question.id, object_id = \
+                        studentObj.id,flexi_data = ansObj.flexi_data)
+
                         ansForm=AnswerForm(request.POST,request,queryset=ansObjs)
                         if not firstuserdata1.count() == 0:
                             f1 = firstuserdata1[0]
-                            f1.answer_score = ansObj.answer_score
-                            f1.answer_grade = ansObj.answer_grade
-                            f1.save()
-                            print "existing record id is", f1.id
+                            if f1.flexi_data != primaryfieldVal and not ansflexivals:
+                                f1.answer_score = ansObj.answer_score
+                                f1.answer_grade = ansObj.answer_grade
+                                f1.flexi_data = ansObj.flexi_data
+                                f1.save()
+                            elif f1 and f1.flexi_data == primaryfieldVal:
+                                f1.answer_score = ansObj.answer_score
+                                f1.answer_grade = ansObj.answer_grade
+                                f1.save()
+                            else:
+                                return 'Primary Value is already existing  '
                         else:
                             if ansForm.errors:
                                if ansForm.errors[0].has_key('__all__'):
@@ -283,13 +293,23 @@ class KLP_ChangeAns(Resource):
                         firstuserdata = Answer.objects.filter(question__id=\
                         question.id, user1__id = user.id, object_id = \
                         studentObj.id, flexi_data = primaryfieldVal)
+
+                        ansflexivals2 = Answer.objects.filter(question__id=\
+                        question.id, object_id = \
+                        studentObj.id,flexi_data = primaryfieldVal)
+                        
+                        try:
+                            fdata = Answer.objects.get(question__id=question.id,\
+                        object_id = studentObj.id, flexi_data = primaryfieldVal)
+                        except:
+                            fdata=''
                         if not firstuserdata.count() == 0:
                             f =  firstuserdata[0]
                             f.answer_score = answer_score
                             f.answer_grade = answer_grade
                             f.save()
-                            print "existing record id is", f.id
                             
+
                         else:
                             ansForm=AnswerForm(newanswerdata,request)
 
@@ -304,79 +324,107 @@ class KLP_ChangeAns(Resource):
                         #ansObj.save()
                     except IntegrityError:
                         return 'Primary  Value is already existing'
-            ansIds.append(str(ansObj.id))      
+            try:
+                ansIds.append(str(ansObj.id))      
+            except:
+                pass
         return 'Data(s) Saved |'+','.join(ansIds)  # ,question.id,',',studentObj.id,',',dE,',',status,',',answer_grade,answer_score,user.id
+
+
 
 
 def KLP_DataValidation(request):
     """ To Validate data for Doble Entry answer/data/validation/"""
-    #import pdb 
-    #pdb.set_trace()
+    
     validateId = request.POST.get('validateField')  #  Get ValidateId (student and question id)
     validateValue = request.POST.get('validateValue')  #  Get Text field value to validate
     listIds = validateId.split('_')
     AnsId=request.POST.get('ansId')
     # Query Answer object based on student and question id
-    if AnsId:
+    print "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", AnsId
+    if AnsId != "None":
        ansObj=Answer.objects.filter(id=AnsId).defer('object_id'
             , 'user1', 'user2', 'last_modified_by')[0]
     else: 
       ansObj = Answer.objects.filter(object_id=listIds[2],
                                    question__id=listIds[1]).defer('object_id'
-            , 'user1', 'user2', 'last_modified_by')[0]
+            , 'user1', 'user2', 'last_modified_by')
+      if ansObj:
+          ansObj=ansObj[0]
     respStr = False
-    dEntry = int(ansObj.double_entry)  # reads dE value
-    
-    if dEntry in [0, 2]:
+    if ansObj:
+        dEntry = int(ansObj.double_entry)  # reads dE value
+        
+        if dEntry in [0, 2]:
 
-        # if dEntry in 0 0r 2 return true
+            # if dEntry in 0 0r 2 return true
 
-        respStr = True
-    else:
-
-        # else check text field value
-
-        if validateValue:
-            if validateValue.lower() == 'ab':
-
-                # If text field value is ab(absent) and answer status is -99999 then return true
-
-                if ansObj.status == -99999:
-                    respStr = True
-            elif validateValue.lower() == 'uk':
-
-                # If text field value is uk(unknown) and answer status is -1 then return true
-
-                if ansObj.status == -1:
-                    respStr = True
-            elif ansObj.question.question_type == 2:
-                if 1:
-
-                        # If question type is 2(Grade) then match with answer grade if matches return true
-
-                    if ansObj.answer_grade.lower() \
-                        == validateValue.lower():
-                        respStr = True
-                    elif (ansObj.answer_grade) == (validateValue):
-                        respStr = True
-                else:
-                    pass
-            else:
-                if 1:
-
-                        # If question type is 1(marks) then match with answer score if matches return true
-                    
-
-                    if float(ansObj.answer_score) \
-                        == float(validateValue):
-                        respStr = True
-                else:
-                    pass
+            respStr = True
         else:
-            respStr = False
+
+            # else check text field value
+
+            if validateValue:
+                if validateValue.lower() == 'ab':
+
+                    # If text field value is ab(absent) and answer status is -99999 then return true
+
+                    if ansObj.status == -99999:
+                        respStr = True
+                elif validateValue.lower() == 'uk':
+
+                    # If text field value is uk(unknown) and answer status is -1 then return true
+
+                    if ansObj.status == -1:
+                        respStr = True
+                elif ansObj.question.question_type == 2:
+                    if 1:
+
+                            # If question type is 2(Grade) then match with answer grade if matches return true
+
+                        if ansObj.answer_grade.lower() \
+                            == validateValue.lower():
+                            respStr = True
+                        elif (ansObj.answer_grade) == (validateValue):
+                            respStr = True
+                    else:
+                        pass
+                else:
+                    if 1:
+
+                            # If question type is 1(marks) then match with answer score if matches return true
+                        
+
+                        if float(ansObj.answer_score) \
+                            == float(validateValue):
+                            respStr = True
+                    else:
+                        pass
+            else:
+                respStr = False
+    else:
+        pass
     return HttpResponse(simplejson.dumps(respStr))
+
+def KLP_getAnswers(request):
+    resp=False
+    aid = request.GET.get('aid')  #  Get Assessment id
+    obid = request.GET.get('obid')
+    print "Assessment id", aid
+    try:
+        asobj = Answer.objects.filter(object_id=obid,question__assessment__id=aid)
+    except:
+        asobj = ''
+    print "hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh", resp
+    if asobj:
+        resp = True
+    else:
+        pass
+    return HttpResponse(simplejson.dumps(resp))
 
 
 urlpatterns = patterns('', url(r'^answer/data/entry/$', KLP_DataEnry),
                        url(r'^answer/data/validation/$',
-                       KLP_DataValidation))
+                       KLP_DataValidation),
+                       url(r'^klp/getanswers/$',KLP_getAnswers)
+                       )
